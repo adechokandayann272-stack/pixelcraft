f551abebe807a7f8270c5648a8cad7c99a13d923
adechokandayann272-stack <adechokandayann272@gmail.com>
2026-08-02 00:43:51 +0200

    Change grid layout from 3 to 4 columns

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -78,7 +78,7 @@ export default function Services() {
         </div>
 
         {/* Services grid */}
-        <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+        <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
           {services.map((service, i) => (
             <div
               key={service.title}
