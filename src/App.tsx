04f7ef883d439733f5c67031a692bcfb15a8228a
adechokandayann272-stack <adechokandayann272@gmail.com>
2026-08-02 14:23:52 +0200

    Replace HashRouter with BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -6,7 +6,7 @@ import PrivacyPolicy from './pages/PrivacyPolicy';
 
 export default function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <div className="min-h-screen flex flex-col">
         <Navbar />
         <main className="flex-1">
@@ -17,6 +17,6 @@ export default function App() {
         </main>
         <Footer />
       </div>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
