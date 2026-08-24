ea2714e810a8de40b4bd2fbc1befaf343ef14ef5
adechokandayann272-stack <adechokandayann272@gmail.com>
2026-08-24 20:12:46 +0200

    Add Process component to Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,7 +9,7 @@ export default function Home() {
     <>
       <Hero />
       <Services />
-      <Process /> {/* <-- SECTION RAJOUTÉE ICI */}
+      <Process /> 
       <Portfolio />
       <Contact />
     </>
